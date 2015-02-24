1173d51b46c8d31f917c48a5bf78b559af96e75c
mori0501 <td_hk_m0501@yahoo.co.jp>
2015-02-25 02:18:23 +0900

    add vhd

--- fsqrt.c
+++ fsqrt.c
@@ -5,7 +5,7 @@
 #include <math.h>
 #include <time.h>
 
-#define INFILE "fsqrt_table.txt"
+#define INFILE "new_sqrt_table.txt"
 #define TESTS 2000000
 #define IS_TIME_RAND 3 // 1ならsrand(time(NULL)) 0でも1でもなければsrand(IS_TIME_RAND)する
 //2 仮数部 all 1 だめ
@@ -154,108 +154,5 @@ uint32_t fsqrt(uint32_t i){
   }
   uint32_t ans = make_ans(sign,exp,downto(mant,22,0));
   return ans;
-}
-
-char *makebinary(){
-  char *ret = (char*)malloc(sizeof(char)*50);
-  int i;
-  for(i=0;i<32;i++){
-    int ra=rand()%2;
-    if(i == 0 || i == 2 || i == 3 || i == 4 || i == 5 || i == 6 || i == 7){
-      ret[i] = '0';
-    }else if(i == 1){
-      ret[i] = '1';
-    }
-    else{
-      if(ra == 1)
-	ret[i] = '1';
-      else if(ra == 0)
-	ret[i] = '0';
-    }
-  }
-  ret[32] = '\n';
-  for(i=33;i<50;i++){
-    ret[i] = 0;
-  }
-  char temp[9];
-  for(i=0;i<8;i++){
-    temp[i] = ret[1+i];
-  }
-  temp[8] = 0;
-  if(strcmp(temp,"00000000") == 0 || strcmp(temp,"11111111") == 0  || strncmp(ret,"1",1) == 0 ){
-    free(ret);
-    ret = makebinary();
-  }
-  return ret;
-}
-int main(int argc,char *argv[]){
-  u2f ans,temp2;
-  if(argc != 2){
-    if(IS_TIME_RAND == 1){
-      srand(time(NULL));
-    }else if(IS_TIME_RAND != 0){
-      srand((unsigned)IS_TIME_RAND);
-    }
-  }else{
-    unsigned int time_rand = atoi(argv[1]);
-    srand(time_rand);
-  }
-  int i;
-  int count[17] = {0};
-  int distance;
-  int max=0,min=0;
-  char my_maxstr[50]={0},my_minstr[50]={0};
-  char ans_max[50]={0},ans_min[50]={0};
-  char quest_max[50]={0},quest_min[50]={0};
-  for(i=0;i<TESTS;i++){
-    char *random = makebinary();
-    uint32_t ux = binarytouint(random);
-    ans.uuu = ux;
-    uint32_t finv_answer = temp2.uuu = fsqrt(ux);
-    //  printf("finv     : %s : %f\n",uinttobinary(finv_answer),temp2.ftemp);
-    float fx = ans.fff;
-    ans.fff = sqrtf(fx);
-    distance = finv_answer - ans.uuu;
-    if(distance <= -8){
-      count[0]++;
-    }
-    else if(distance >= 8){
-      count[16]++;
-    }
-    else{
-      count[distance+8]++;
-    }
-    //if(distance > 500 || distance < -500)
-    // continue; // exp が0x00または0xffのときおかしくなるので
-    if(max < distance){
-      max = distance;
-      strcpy(my_maxstr,uinttobinary(finv_answer));
-      strcpy(ans_max,uinttobinary(ans.uuu));
-      strcpy(quest_max,random);
-    }
-    else if(min > distance){
-      min = distance;
-      strcpy(my_minstr,uinttobinary(finv_answer));
-      strcpy(ans_min,uinttobinary(ans.uuu));
-      strcpy(quest_min,random);
-    }
-    //printf("answer   : %s : %f\n",uinttobinary(temp.utemp),temp.ftemp);
-    free(random);
-  }
-  for(i=0;i<17;i++){
-    printf("count[%d] = %d\n",i-8,count[i]);
-  }
-  printf("指数部が0x00または0xffのとき除外している。\n");
-  printf("max = %d\n",max);
-  printf("fsqrt : %s\n",my_maxstr);
-  printf("ansx  : %s\n",ans_max);
-  printf("qmax  : %s",quest_max);
-  printf("debug :  EXP     FRA    89\n\n");
-  printf("min = %d\n",min);
-  printf("fsqrt : %s\n",my_minstr);
-  printf("sqrtf : %s\n",ans_min);
-  printf("qmin  : %s\n",quest_min);
-  printf("debug :  EXP     FRA    89 \n");
-  return 0;
 }
 
